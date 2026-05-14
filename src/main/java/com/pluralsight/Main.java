b2e1cfb026ad145ebd7174c33c71c2fec7142218
edomg <edomgezahegn54@gmail.com>
2026-05-13 21:49:06 -0500

    Update main to create vehicles with constructors

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -5,55 +5,25 @@ public class Main {
 
         public static void main(String[] args) {
 
-            // create a moped object
-            Moped slowRide = new Moped();
+            Moped slowRide = new Moped("Red", 1, 1, 5);
+            Car familyCar = new Car("Blue", 5, 4, 15);
+            SemiTruck bigTruck = new SemiTruck("Black", 2, 5000, 100);
+            Hovercraft futureRide = new Hovercraft("Silver", 6, 10, 20);
 
-            // use inherited setters
-            slowRide.setColor("Red");
-            slowRide.setFuelCapacity(5);
-            slowRide.setNumberOfPassengers(1);
-            slowRide.setCargoCapacity(1);
-
-            // print values using inherited getters
             System.out.println("Moped Color: " + slowRide.getColor());
-            System.out.println("Fuel Capacity: " + slowRide.getFuelCapacity());
-
-
-
-            // create a car object
-            Car familyCar = new Car();
-
-            familyCar.setColor("Blue");
-            familyCar.setFuelCapacity(15);
-            familyCar.setNumberOfPassengers(5);
-            familyCar.setCargoCapacity(4);
+            System.out.println("Moped Fuel Capacity: " + slowRide.getFuelCapacity());
 
             System.out.println("Car Color: " + familyCar.getColor());
-            System.out.println("Passengers: " + familyCar.getNumberOfPassengers());
+            System.out.println("Car Passengers: " + familyCar.getNumberOfPassengers());
 
-
-
-            // create a semi truck object
-            SemiTruck bigTruck = new SemiTruck();
-
-            bigTruck.setColor("Black");
-            bigTruck.setFuelCapacity(100);
-            bigTruck.setNumberOfPassengers(2);
-            bigTruck.setCargoCapacity(5000);
-
-            System.out.println("Truck Cargo Capacity: " + bigTruck.getCargoCapacity());
-
-
-
-            // create a hovercraft object
-            Hovercraft futureRide = new Hovercraft();
-
-            futureRide.setColor("Silver");
-            futureRide.setFuelCapacity(20);
-            futureRide.setNumberOfPassengers(6);
-            futureRide.setCargoCapacity(10);
+            System.out.println("Semi Truck Cargo Capacity: " + bigTruck.getCargoCapacity());
 
             System.out.println("Hovercraft Color: " + futureRide.getColor());
         }
-    }
+
+
+
+
+        }
+
 
