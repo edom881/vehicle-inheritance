8e616cadea826cdfbe6ec1e90f3b9c55578a5466
edomg <edomgezahegn54@gmail.com>
2026-05-13 21:41:56 -0500

    Add child class constructors using super

--- src/main/java/com/pluralsight/SemiTruck.java
+++ src/main/java/com/pluralsight/SemiTruck.java
@@ -2,4 +2,10 @@ package com.pluralsight;
 
 public class SemiTruck extends Vehicle {
 
+    public SemiTruck(String color, int numberOfPassengers,
+                     int cargoCapacity, int fuelCapacity) {
+
+        super(color, numberOfPassengers, cargoCapacity, fuelCapacity);
+    }
+
 }
