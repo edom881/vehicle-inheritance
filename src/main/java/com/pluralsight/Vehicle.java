de3898229e9b5d2bb12c9145b8c128ed635d076c
edomg <edomgezahegn54@gmail.com>
2026-05-13 20:54:04 -0500

    Add Vehicle parent class with getters and setters

---
+++ src/main/java/com/pluralsight/Vehicle.java
@@ -0,0 +1,51 @@
+package com.pluralsight;
+
+public class Vehicle {
+
+        // shared properties for all vehicles
+        private String color;
+        private int numberOfPassengers;
+        private int cargoCapacity;
+        private int fuelCapacity;
+
+        // getter for color
+        public String getColor() {
+            return color;
+        }
+
+        // setter for color
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        // getter for passengers
+        public int getNumberOfPassengers() {
+            return numberOfPassengers;
+        }
+
+        // setter for passengers
+        public void setNumberOfPassengers(int numberOfPassengers) {
+            this.numberOfPassengers = numberOfPassengers;
+        }
+
+        // getter for cargo capacity
+        public int getCargoCapacity() {
+            return cargoCapacity;
+        }
+
+        // setter for cargo capacity
+        public void setCargoCapacity(int cargoCapacity) {
+            this.cargoCapacity = cargoCapacity;
+        }
+
+        // getter for fuel capacity
+        public int getFuelCapacity() {
+            return fuelCapacity;
+        }
+
+        // setter for fuel capacity
+        public void setFuelCapacity(int fuelCapacity) {
+            this.fuelCapacity = fuelCapacity;
+        }
+    }
+
